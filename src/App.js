123cc5056ef756c296dd2d9220c8ab09ca8ce7ec
halilibrahimcelik <halil.celik139@gmail.com>
2022-07-25 16:18:42 +0300

    Added prepare callback fn to postSlice

--- src/App.js
+++ src/App.js
@@ -1,7 +1,9 @@
+import AddPostForm from "./components/AddPostForm";
 import PostList from "./components/PostList";
 function App() {
   return (
     <main className="App">
+      <AddPostForm />
       <PostList />
     </main>
   );
